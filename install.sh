64c47136e71a5417e75919fbd35268854e595a28
Olivier Perez [a570709] <olivier.perez@worldline.com>
2018-09-17 11:25:21 +0000

    Check if user is root at install

--- install.sh
+++ install.sh
@@ -2,41 +2,72 @@
 
 cd
 
-apt-get update
+function root_install {
+        apt-get update
 
-apt-get install -y \
+        apt-get install -y \
+                curl \
+                vim \
+                screen \
+                htop \
+                ntp \
+                git \
+
+
+        # Docker
+        apt-get install -y \
+                apt-transport-https \
+                ca-certificates \
+                software-properties-common
+
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+        apt-key fingerprint 0EBFCD88
+
+        add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+        apt-get install -y docker-ce
+        groupadd dockers
+        gpasswd -a olivier docker
+        service docker restart
+}
+
+function curl_dotfiles {
 	curl \
-	vim \
-	screen \
-	htop \
-	ntp \
-	git \
+	        -O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.aliases.docker \
+	        -O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.aliases.global \
+	        -O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.bashps1 \
+	        -O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.gitconfig \
+	        -O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.screenrc \
+	        -O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.vimrc
+
+	echo ". ~/.aliases.global" >> ~/.bashrc
+	echo ". ~/.aliases.docker" >> ~/.bashrc
+}
+
+cd
+
+if [ "$(id -u)" != "0" ]; then
+
+        echo "------------------------------------------"
+        echo "You need to be root to do the full install"
+        echo "------------------------------------------"
+
+        curl_dotfiles
 
 
-curl \
-	-O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.aliases.docker \
-	-O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.aliases.global \
-	-O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.bashps1 \
-	-O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.gitconfig \
-	-O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.screenrc \
-	-O https://raw.githubusercontent.com/olivierperez/dotfiles/master/files/.vimrc
+else
 
-echo ". ~/.aliases.global" >> ~/.bashrc
-echo ". ~/.aliases.docker" >> ~/.bashrc
+        echo "------------------"
+        echo "Installing as root"
+        echo "------------------"
 
-# Docker
-apt-get install -y \
-	apt-transport-https \
-	ca-certificates \
-	software-properties-common
+        root_install
+        curl_dotfiles
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+fi
 
-apt-key fingerprint 0EBFCD88
+echo "---------------------"
+echo "Installation finished"
+echo "---------------------"
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt-get install -y docker-ce
-groupadd dockers
-gpasswd -a olivier docker
-service docker restart
